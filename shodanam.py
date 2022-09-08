4567272bdba7c782ec673e9c472c922951ab87ec
777388 <96343159+777388@users.noreply.github.com>
2022-09-07 20:08:19 -0700

    Update shodanam.py

--- shodanam.py
+++ shodanam.py
@@ -1,13 +1,13 @@
 import os
 import sys
 
-i = 'in.txt'
-o = open('out.txt', 'w')
+i = sys.argv[3]
+o = open(sys.argv[4], 'w')
 query = sys.argv[2]
 prog = sys.argv[1]
-print("usage: python3 shodanam.py pathtoprogram query")
-print("example: python3 shodanam.py \"enum4linux -a\" \"Authentication: disabled' port:445 database\"")
-print("python3 shodanam.py \"nmap -v -A\" \"board of\"")
+print("usage: python3 shodanam.py pathtoprogram query in.txt out.txt")
+print("example: python3 shodanam.py \"enum4linux -a\" \"Authentication: disabled' port:445 database\" in.txt out.txt")
+print("python3 shodanam.py \"nmap -v -A\" \"board of\" in.txt out.txt")
 
 os.system('shodan download '+i+' '+query)
 os.system('shodan parse --fields ip_str '+i+'.json.gz > '+i)
