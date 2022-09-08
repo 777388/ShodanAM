5ac512637d1c6da1abe7249990d46007bf85c60e
777388 <96343159+777388@users.noreply.github.com>
2022-09-07 18:51:55 -0700

    Update shodanam.py

--- shodanam.py
+++ shodanam.py
@@ -11,6 +11,7 @@ prog = sys.argv[1]
 options = sys.argv[2]
 print("usage: python3 shodanam.py pathtoprogram options query")
 print("example: python3 shodanam.py enum4linux '-a' `'Authentication: disabled' port:445 database`")
+print("python3 shodanam.py nmap '-v -A' 'board of'")
 
 os.system('shodan download '+i+' '+query)
 os.system('shodan parse --fields ip_str '+i+'.json.gz > '+i)
