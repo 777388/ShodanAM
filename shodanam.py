40cf5bcb9bd1e89198a3d3867bcd8a0d5ee2c873
777388 <96343159+777388@users.noreply.github.com>
2022-09-07 18:33:39 -0700

    ShodanAM.py
    Usage: python3 shodanam.py pathtoprogram options shodanquery

---
+++ shodanam.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+def func(value):
+    return ''.join(value.splitlines())
+i = in.txt
+b = '200'
+o = open(out.txt, 'w')
+query = sys.argv[3]
+prog = sys.argv[1]
+options = sys.argv[2]
+print("usage: python3 shodanam.py pathtoprogram options query")
+
+os.system('shodan download '+i+' '+query)
+os.system('shodan parse --fields ip_str '+i+'.json.gz > '+i)
+with open(i, 'r') as file:
+    for line in file:
+        thing = os.popen(prog+' '+options+' '+line).read()
+        print(thing, file=o)
+
