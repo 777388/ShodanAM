60cc57eb4390804c8b8f2ef613fd8c85555d8f65
777388 <96343159+777388@users.noreply.github.com>
2022-09-07 19:58:27 -0700

    Update shodanam.py

--- shodanam.py
+++ shodanam.py
@@ -3,18 +3,16 @@ import sys
 
 i = 'in.txt'
 o = open('out.txt', 'w')
-query = sys.argv[3]
+query = sys.argv[2]
 prog = sys.argv[1]
-options = sys.argv[2]
-print("usage: python3 shodanam.py pathtoprogram options query")
-print("example: python3 shodanam.py enum4linux '-a' `'Authentication: disabled' port:445 database`")
-print("python3 shodanam.py nmap '-v -A' 'board of'")
+print("usage: python3 shodanam.py pathtoprogram query")
+print("example: python3 shodanam.py \"enum4linux -a\" \"Authentication: disabled' port:445 database\"")
+print("python3 shodanam.py \"nmap -v -A\" \"board of\"")
 
 os.system('shodan download '+i+' '+query)
 os.system('shodan parse --fields ip_str '+i+'.json.gz > '+i)
 with open(i, 'r') as file:
     for line in file:
-        thing = os.popen(prog+' '+options+' '+line).read()
+        thing = os.popen(prog+' '+line).read()
         print(thing)
         print(thing, file=o)
-
